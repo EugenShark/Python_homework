fe8f79086e4a0c9878ae5c32ec7f35573eb29c06
Evgeniy Sharko <sharkoeugene@gmail.com>
2020-04-14 17:49:54 +0300

    Task 1 done

---
+++ task1.py
@@ -0,0 +1,9 @@
+year = 2020
+month = 'April'
+time = '17:34'
+print(f"{month} of {year}, {time}")
+print('Привет!')
+name = input('Введите имя: ')
+print(f"Добро пожаловать, {name}!")
+age = input('Введите возраст: ')
+print(f"Тебе повезло, {name}, в {age} все еще только начинается!")
