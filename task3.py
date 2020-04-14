e34a5c515be758a7bc79e8398e955b4753377d94
Evgeniy Sharko <sharkoeugene@gmail.com>
2020-04-14 21:26:53 +0300

    Task 3 done

---
+++ task3.py
@@ -0,0 +1,6 @@
+print('Сумминатор')
+num = (input('Введите число: '))
+sum1 = int(num + num)
+sum2 = int(num + num + num)
+su = int(num) + sum1 + sum2
+print(f"Ваша сумма: {num} + {num + num} + {num + num + num} = {su}")
