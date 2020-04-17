a191a4999d261f6d13914be73ccf459320ab192d
Evgeniy Sharko <sharkoeugene@gmail.com>
2020-04-17 17:24:16 +0300

    Fix Task 3

--- task3.py
+++ task3.py
@@ -3,4 +3,4 @@ num = (input('Введите число: '))
 sum1 = int(num + num)
 sum2 = int(num + num + num)
 su = int(num) + sum1 + sum2
-print(f"Ваша сумма: {num} + {num + num} + {num + num + num} = {su}")
+print(f"Ваша сумма: {num} + {sum1} + {sum2} = {su}")
